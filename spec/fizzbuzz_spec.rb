badd2c65af6f288a2ad8c14c065b4c66618a5cab
Matt <mdwareing@gmail.com>
2018-05-08 14:18:10 +0100

    Test 2

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,11 @@
 require 'fizzbuzz'
 
-describe fizzbuzz do
+describe 'fizzbuzz' do
   it "return 'fizz' when given 3" do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(3)).to eq "fizz"
+  end
+
+  it "return 'buzz' when given 5" do
+    expect(fizbuzz(5)).to eq "buzz"
   end
 end
